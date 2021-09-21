1c2d300d7eb57b0935d5652da5c74c31734122f4
anh0034 <hayandrew2@gmail.com>
2021-09-21 02:31:27 -0400

    Initial commit

---
+++ src/main/Body/second-bar.component.ts
@@ -0,0 +1,7 @@
+import { Component, VERSION } from '@angular/core';
+@Component({
+selector: 'app-second-bar',
+templateUrl: './second-bar.component.html',
+styleUrls: ['./second-bar.component.css'],
+})
+export class SecondBarComponent {}
---
+++ src/main/Layout/layout-top-body.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector:'app-layout-top-body',
+  templateUrl: './layout-top-body.component.html',
+  styleUrls: ['./layout-top-body.component.css'],
+})
+export class LayoutTopBody {
+
+}
